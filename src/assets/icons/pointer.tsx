43692ae79187666ae0cf5f245ebc406cc307e5ad
bekzod9936 <bekzodnorkulov9936@gmail.com>
2023-04-10 19:36:28 +0500

    fix: delete pointer svg

--- src/assets/icons/pointer.tsx
+++
@@ -1,7 +0,0 @@
-export const IconPointer = () => {
-	return (
-		<svg width='39' height='38' viewBox='0 0 39 38' fill='none' xmlns='http://www.w3.org/2000/svg'>
-			<path d='M38.5 0L0 14C20.8 15.2 24 30.5 23 38L38.5 0Z' fill='#857FFF' />
-		</svg>
-	)
-}
